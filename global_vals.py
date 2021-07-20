2920cb9643cfe75417592e82f37fb32b4d52e5d4
clair <clair.cui@loratechai.com>
2021-07-20 15:45:36 +0800

    align period_end based on available stock_return

--- global_vals.py
+++ global_vals.py
@@ -11,7 +11,7 @@ factor_premium_table = "ai_factor_factor_premium"
 
 # TABLE names - raw data
 dl_value_universe_table = "universe"
-worldscope_quarter_summary_table = "data_worldscope_summary_test"
+worldscope_quarter_summary_table = "data_worldscope_summary"
 ibes_data_table = "data_ibes_monthly"
 macro_data_table = "data_macro_monthly"
 stock_data_table = "master_ohlcvtr"
