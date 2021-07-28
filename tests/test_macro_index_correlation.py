206c1a71547f67a231f32c414e18eff3b974a3b4
clair <clair.cui@loratechai.com>
2021-07-28 14:30:01 +0800

    update rf

--- tests/test_macro_index_correlation.py
+++ tests/test_macro_index_correlation.py
@@ -2,6 +2,9 @@ from preprocess.load_data import combine_data, download_index_return
 import global_vals
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 def get_index_return():
 
@@ -13,14 +16,38 @@ def get_index_return():
     # db_table_name = global_vals.processed_ratio_table
 
     with global_vals.engine_ali.connect() as conn:
-        index_ret = pd.read_sql(f"SELECT ticker, return_1yr "
-                                f"FROM {db_table_name} WHERE ticker like '.%%'", conn)
+        index_ret = pd.read_sql(f"SELECT * FROM {db_table_name} WHERE ticker like '.%%'", conn)
     global_vals.engine_ali.dispose()
 
-    stock_return_col = ['stock_return_r1_0', 'stock_return_r6_2', 'stock_return_r12_7']
-    index_ret[stock_return_col] = index_ret[stock_return_col] + 1
-    index_ret['return_1yr'] = np.prod(index_ret[stock_return_col].values, axis=1) - 1
-    index_ret = pd.pivot_table(index_ret, columns=['ticker'], index=['period_end'], values='return').reset_index(drop=False)
+    index_ret = index_ret.loc[index_ret['ticker']!='.HSLI']
+
+    fig = plt.figure(figsize=(12, 12), dpi=120, constrained_layout=True)
+    writer = pd.ExcelWriter(f'eda/index_corr.xlsx')
+
+    k=1
+    for i in index_ret.columns.to_list()[2:]:
+        ax = fig.add_subplot(3, 3, k)
+        g = index_ret[['period_end', 'ticker', i]]
+        g = g.set_index(['period_end', 'ticker']).unstack()
+        g.columns = [x[1] for x in g.columns.to_list()]
+        g = g.reset_index().drop(['period_end'], axis=1)
+
+        cr = g.astype(float).corr()
+        sns.set(style="whitegrid", font_scale=0.5)
+        ax = sns.heatmap(cr, cmap='PiYG', vmin=-1, vmax=1, label='small')
+        ax.set_title(i, fontsize=20)
+        k += 1
+
+        cr_df = cr.stack(-1)
+        cr_df = cr_df.reset_index()
+        cr_df.columns=['f1','f2','corr']
+        cr_df = cr_df.loc[cr_df['corr']!=1].drop_duplicates(subset=['corr'])
+        cr_df['corr_abs'] = cr_df['corr'].abs()
+        cr_df = cr_df.sort_values(by=['corr_abs'], ascending=False)
+        cr_df.to_excel(writer, sheet_name=i)
+
+    plt.savefig('eda/index_corr.png', dpi=300)
+    writer.save()
 
 if __name__ == '__main__':
     df = get_index_return()
