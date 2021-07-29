757e4814c7762798ba30f7d811a70e45098b7156
clair <clair.cui@loratechai.com>
2021-07-29 11:00:59 +0800

    update rf result analysis

--- results_analysis/rf_score_merge.py
+++ results_analysis/rf_score_merge.py
@@ -26,9 +26,9 @@ def download_stock_pred():
     # print('Last running for factors ', df['y_type'].to_list()[-1])
     # print(f"Running time {df['finish_timing'].max() - df['finish_timing'].min()}. From {df['finish_timing'].min()} to {df['finish_timing'].max()}")
     y_type_list = df['y_type'][0][1:-1].split(',')
-    train_acc_list = [(x, 'train') for x in y_type_list]
-    valid_acc_list = [(x, 'valid') for x in y_type_list]
-    test_acc_list = [(x, 'test') for x in y_type_list]
+    train_acc_list = [(x+'/train') for x in y_type_list]
+    valid_acc_list = [(x+'/valid') for x in y_type_list]
+    test_acc_list = [(x+'/test') for x in y_type_list]
 
     df[train_acc_list] = df['accuracy_train'].apply(lambda x: x[1:-1]).str.split(',', expand=True).astype(float)
     df[valid_acc_list] = df['accuracy_valid'].apply(lambda x: x[1:-1]).str.split(',', expand=True).astype(float)
@@ -37,21 +37,22 @@ def download_stock_pred():
     writer = pd.ExcelWriter(f'score/rf_result_score_accuracy_{iter_name}.xlsx')
 
     # 1. select results with best average accuracy_valid_mean
-    # best = df.loc[df.groupby(['group_code','testing_period','cv_number'])['accuracy_valid_mean'].transform(max)==df['accuracy_valid_mean']]
-    # best = best[['group_code'] + train_acc_list + valid_acc_list + test_acc_list]
-    # best_cv_time = best.groupby(['group_code']).mean().transpose()
-    #
-    # best_id = best_cv_time.index.to_frame().reset_index(drop=True)
-    # best_cv_time.index = pd.MultiIndex.from_tuples(best_id[0].tolist())
-    # best_cv_time = best_cv_time.transpose().stack(0).reset_index()
-    #
-    # best_cv_time.to_excel(writer, sheet_name='best_avg_valid_mean', index=False)
+    best = df.loc[df.groupby(['group_code','testing_period','cv_number'])['accuracy_valid_mean'].transform(max)==df['accuracy_valid_mean']]
+    best = best[['group_code'] + train_acc_list + valid_acc_list + test_acc_list]
+    best_cv_time = best.groupby(['group_code']).mean().transpose()
+
+    best_id = best_cv_time.index.to_frame().reset_index(drop=True)
+    x = pd.MultiIndex.from_arrays(best_id[0].str.split('/', expand=True).tranpose().values)
+    best_cv_time.index = pd.MultiIndex.from_arrays(best_id[0].str.split('/', expand=True).tranpose().values)
+    best_cv_time = best_cv_time.transpose().stack(0).reset_index()
+
+    best_cv_time.to_excel(writer, sheet_name='best_avg_valid_mean', index=False)
 
     # 2. select results with best accuracy_valid for each y_type
     df_list = []
-    for i in range(len(y_type_list)):
-        best = df.loc[df.groupby(['group_code','testing_period','cv_number'])[(i, 'valid')].transform(max)==df[(i, 'valid')]]
-        best = best[['group_code', (i, 'train'), (i, 'valid'), (i, 'test')]]
+    for i in y_type_list:
+        best = df.loc[df.groupby(['group_code','testing_period','cv_number'])[(i,'valid')].transform(max)==df[(i,'valid')]]
+        best = best[['group_code', [(i,'train')], [(i,'valid')], [(i,'test')]]]
         best_cv_time = best.groupby(['group_code']).mean()
         best_cv_time.columns = ['train','valid','test']
         df_list.append(best_cv_time)
