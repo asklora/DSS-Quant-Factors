94685eda9db57de215208b1623e8971d533fe2a6
loratech <lora0218G>
2022-06-24 17:32:11 +0800

    change load configs train

--- components/model_evaluation/tests/test_load_configs.py
+++ components/model_evaluation/tests/test_load_configs.py
@@ -1,12 +1,12 @@
 def test_get_latest_name_sql():
-    from components.model_evaluation.src.load_configs import load_latest_name_sql
+    from components.model_evaluation.src.load_eval_configs import load_latest_name_sql
     name = load_latest_name_sql(8)
 
     assert type(name) != type(None)
 
 
 def test_load_eval_config():
-    from components.model_evaluation.src.load_configs import load_eval_config
+    from components.model_evaluation.src.load_eval_configs import load_eval_config
     df = load_eval_config(4)
 
     assert len(df) > 0
